10ec9a1da8f10ccdbab0e45259e607e1a2235580
Mattkx9 <Mattkx9@gmail.com>
2014-08-11 07:58:32 -0500

    Edited build.gradle to 1.7.10
    Also changed version in all the code to v2.1.0

--- src/main/java/io/github/mattkx4/morefurnaces/lib/Strings.java
+++ src/main/java/io/github/mattkx4/morefurnaces/lib/Strings.java
@@ -3,5 +3,5 @@ package io.github.mattkx4.morefurnaces.lib;
 public class Strings {
 	public static final String MODID = "mfm";
 	public static final String name = "Mo' Furnaces Mod";
-	public static final String version = "ALPHA V0.1";
+	public static final String version = "v2.1.0";
 }
