5d5909b72e441e18dec422d09b9dcbe40f537cac
Mattkx4 <Mattkx9@gmail.com>
2014-10-27 20:57:38 -0500

    Added multipliers and percentages for furnaces
    This is to help with development of the wiki.

--- src/main/java/io/github/mattkx4/morefurnaces/lib/FurnaceVariables.java
+++ src/main/java/io/github/mattkx4/morefurnaces/lib/FurnaceVariables.java
@@ -5,33 +5,33 @@ public class FurnaceVariables {
 	 * Tier 1 Furnaces
 	 */
 	// SPEEDS
-	public static final int BRICK_FURNACE_SPEED = 133;
-	public static final int IRON_FURNACE_SPEED = 100;
-	public static final int GOLD_FURNACE_SPEED = 50;
-	public static final int NETHERRACK_FURNACE_SPEED = 450;
-	public static final int QUARTZ_FURNACE_SPEED = 200;
-	public static final int DIAMOND_FURNACE_SPEED = 66;
-	public static final int OBSIDIAN_FURNACE_SPEED = 50;
-	public static final int BONE_FURNACE_SPEED = 175;
-	public static final int REDSTONE_FURNACE_SPEED = 200;
-	public static final int ANVIL_FURNACE_SPEED = 250;
-	public static final int CACTUS_FURNACE_SPEED = 100;
+	public static final int BRICK_FURNACE_SPEED = 133; // 1.5x Faster or 150% Faster
+	public static final int IRON_FURNACE_SPEED = 100; // 2x Faster or 200% Faster
+	public static final int GOLD_FURNACE_SPEED = 50; // 4x Faster or 400% Faster
+	public static final int NETHERRACK_FURNACE_SPEED = 450; // Bu derp
+	public static final int QUARTZ_FURNACE_SPEED = 200; // Same Speed
+	public static final int DIAMOND_FURNACE_SPEED = 66; // 3.03x Faster or 303% Faster
+	public static final int OBSIDIAN_FURNACE_SPEED = 50; // 4x Faster or 400% Faster
+	public static final int BONE_FURNACE_SPEED = 175; // 1.14x Faster or 114% Faster
+	public static final int REDSTONE_FURNACE_SPEED = 200; // Same Speed
+	public static final int ANVIL_FURNACE_SPEED = 250; // Bu derp
+	public static final int CACTUS_FURNACE_SPEED = 100; // 2x Faster or 200% Faster
 	
 	// EFFICIENCIES
-	public static final double BRICK_FURNACE_EFFICIENCY = 1.0D;
-	public static final double IRON_FURNACE_EFFICIENCY = 2.0D;
-	public static final double GOLD_FURNACE_EFFICIENCY = 8.0D;
-	public static final double NETHERRACK_FURNACE_EFFICIENCY = 0.0D;
-	public static final double QUARTZ_FURNACE_EFFICIENCY = 0.5D;
-	public static final double DIAMOND_FURNACE_EFFICIENCY = 3.0D;
-	public static final double OBSIDIAN_FURNACE_EFFICIENCY = 8.0D;
-	public static final double BONE_FURNACE_EFFICIENCY = 2.0D;
-	public static final double REDSTONE_FURNACE_EFFICIENCY = 1.0D;
-	public static final double ANVIL_FURNACE_EFFICIENCY = 1.0D;
-	public static final double CACTUS_FURNACE_EFFICIENCY = 2.0D;
+	public static final double BRICK_FURNACE_EFFICIENCY = 1.0D; // Same Efficiency
+	public static final double IRON_FURNACE_EFFICIENCY = 2.0D; // 0.5x Less Efficient or 50% Less Efficient
+	public static final double GOLD_FURNACE_EFFICIENCY = 8.0D; // Bu derp
+	public static final double NETHERRACK_FURNACE_EFFICIENCY = 0.0D; // null
+	public static final double QUARTZ_FURNACE_EFFICIENCY = 0.5D; // 2x More Efficient or 200% More Efficient
+	public static final double DIAMOND_FURNACE_EFFICIENCY = 3.0D; // Bu derp
+	public static final double OBSIDIAN_FURNACE_EFFICIENCY = 8.0D; // Bu derp
+	public static final double BONE_FURNACE_EFFICIENCY = 2.0D; // Bu derp
+	public static final double REDSTONE_FURNACE_EFFICIENCY = 1.0D; // Same Efficiency
+	public static final double ANVIL_FURNACE_EFFICIENCY = 1.0D; // Same Efficiency
+	public static final double CACTUS_FURNACE_EFFICIENCY = 2.0D; // Bu derp
 	
 	// "DAMAGE FIXED" (For AnvilFurnace)
-	public static final int ANVIL_FURNACE_DAMAGE_FIXED = 25;
+	public static final int ANVIL_FURNACE_DAMAGE_FIXED = 25; // This needs no description...
 	
 	/*
 	 * Tier 2 Furnaces
