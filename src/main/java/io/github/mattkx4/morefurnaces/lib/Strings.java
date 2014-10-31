d05edf3c8c06a2360435632e7e63dfe6e13a5189
Mattkx4 <Mattkx9@gmail.com>
2014-10-31 08:25:15 -0500

    Versions have been updated

--- src/main/java/io/github/mattkx4/morefurnaces/lib/Strings.java
+++ src/main/java/io/github/mattkx4/morefurnaces/lib/Strings.java
@@ -3,5 +3,5 @@ package io.github.mattkx4.morefurnaces.lib;
 public class Strings {
 	public static final String MODID = "mfm";
 	public static final String name = "Mo' Furnaces Mod";
-	public static final String version = "2.2.0";
+	public static final String version = "2.2.1";
 }
