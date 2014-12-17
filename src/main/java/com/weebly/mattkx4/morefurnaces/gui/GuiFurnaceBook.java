c65f491d0b56b067a1a5283d94bd62a80ae7edce
Kyle van Dordrecht <noxus_prime@hotmail.com>
2014-12-17 10:20:02 +0800

    Guide Update Menus
    - Added methods to handle programatically adding and removing submenus
    from the guide.

--- src/main/java/com/weebly/mattkx4/morefurnaces/gui/GuiFurnaceBook.java
+++ src/main/java/com/weebly/mattkx4/morefurnaces/gui/GuiFurnaceBook.java
@@ -29,39 +29,91 @@ public class GuiFurnaceBook extends GuiScreen {
 	private int xSize;
 	private int ySize;
 	/** A list of all the buttons in this container. */
-    protected List buttonList = new ArrayList();
+    protected List buttonListMenu = new ArrayList();
+    
+    // A list of the Item sub menu buttons
+    protected List buttonListItems = new ArrayList();
 
     /** The button that was just pressed. */
     private GuiButton selectedButton;
     
+    // Variables for the x, y and f values
+    private int xStored = 0;
+    private int yStored = 0;
+    private float fStored = 0;
+    private int index = 0;
+        
     // Button for closing the book
     private GuiButton buttonClose = new GuiButton(1, 200, 200, 100, 20, "Close");
     // Main menu buttons
-    private GuiButton buttonItems = new GuiButton(2, 100, 10, 20, 20, "Items");
-    private GuiButton buttonFurnaceBase = new GuiButton(3, 100, 30, 20, 20, "Base Furnaces");
-    private GuiButton buttonFurnaceTier = new GuiButton(4, 100, 50, 20, 20, "Tiered Furnaces");
-    private GuiButton buttonFurnaceSpecial = new GuiButton(5, 100, 70, 20, 20, "Special Furnaces");
-    private GuiButton buttonMobs = new GuiButton(6, 100, 90, 20, 20, "Mobs");
+    private GuiButton buttonItems = new GuiButton(2, 100, 10, 50, 20, "Items");
+    private GuiButton buttonFurnaceBase = new GuiButton(3, 150, 10, 50, 20, "Base Furnaces");
+    private GuiButton buttonFurnaceTier = new GuiButton(4, 200, 10, 50, 20, "Tiered Furnaces");
+    private GuiButton buttonFurnaceSpecial = new GuiButton(5, 250, 10, 50, 20, "Special Furnaces");
+    private GuiButton buttonMobs = new GuiButton(6, 300, 10, 50, 20, "Mobs");
+    
+    // Menu buttons for the items
+    private GuiButton buttonItemsTier2 = new GuiButton(21, 100, 10, 20, 20, "Tier 2 Device");
+    private GuiButton buttonItemsTier3 = new GuiButton(22, 100, 30, 20, 20, "Tier 3 Device");
+    private GuiButton buttonItemsGuide = new GuiButton(23, 100, 50, 20, 20, "Furnace Guide");
+    private GuiButton buttonItemsSteelIng = new GuiButton(24, 100, 70, 20, 20, "Steel Ingot");
+    private GuiButton buttonItemsTierCore = new GuiButton(25, 100, 90, 20, 20, "Tier Core");
+    private GuiButton buttonItemsBoneFrag = new GuiButton(26, 100, 110, 20, 20, "Bone Fragment");
+    private GuiButton buttonItemsUpBri = new GuiButton(27, 100, 130, 10, 20, "Upgrade: Brightness");
+    private GuiButton buttonItemsUpDou = new GuiButton(28, 100, 150, 20, 20, "Upgrade: Double Output");
+    private GuiButton buttonItemsUpFue = new GuiButton(29, 100, 170, 20, 20, "Upgrade: Fuel Saver");
+    private GuiButton buttonItemsUpNot = new GuiButton(30, 100, 190, 20, 20, "Upgrade: Notification");
+    private GuiButton buttonItemsUpInp = new GuiButton(31, 100, 210, 20, 20, "Upgrade: Input Timer");
+
+
 
 	
 	
 	public GuiFurnaceBook(EntityPlayer entityPlayer) {
 		this.xSize = 200;
 		this.ySize = 200;
-		buttonList.add(buttonClose);
-		buttonList.add(buttonItems);
-		buttonList.add(buttonFurnaceBase);
-		buttonList.add(buttonFurnaceTier);
-		buttonList.add(buttonFurnaceSpecial);
-		buttonList.add(buttonMobs);
-
+		// Add the main menu buttons
+		buttonListMenu.add(buttonClose);
+		buttonListMenu.add(buttonItems);
+		buttonListMenu.add(buttonFurnaceBase);
+		buttonListMenu.add(buttonFurnaceTier);
+		buttonListMenu.add(buttonFurnaceSpecial);
+		buttonListMenu.add(buttonMobs);
+		
+		// Add the Item sub-menu buttons
+		buttonListItems.add(buttonItemsTier2);
+		buttonListItems.add(buttonItemsTier3);
+		buttonListItems.add(buttonItemsGuide);
+		buttonListItems.add(buttonItemsSteelIng);
+		buttonListItems.add(buttonItemsTierCore);
+		buttonListItems.add(buttonItemsBoneFrag);
+		buttonListItems.add(buttonItemsUpBri);
+		buttonListItems.add(buttonItemsUpDou);
+		buttonListItems.add(buttonItemsUpFue);
+		buttonListItems.add(buttonItemsUpNot);
+		buttonListItems.add(buttonItemsUpInp);
+		
+	}
+	
+	/**
+	 * This method overrides the default draw screen method from the GuiScreen class
+	 * and calls the custom draw screen method below.
+	 */
+	@Override
+	public void drawScreen(int x, int y, float f) {
+		// Store the x, y and f values
+		xStored = x;
+		yStored = y;
+		fStored = f;
+		drawScreen(x, y, f, index);
+		return;
 	}
 	
 	/**
      * Draws the screen and all the components in it.
+	 * @param index 
      */
-	@Override
-    public void drawScreen(int x, int y, float f)
+	public void drawScreen(int x, int y, float f, int index)
     {
 		// Draw the default background
 		drawDefaultBackground();
@@ -73,8 +125,6 @@ public class GuiFurnaceBook extends GuiScreen {
 		int posX = (this.width - xSize) / 2;
 		int posY = (this.height - ySize) / 2;
 
-		
-		
 		// Draw the main GUI background
 		drawTexturedModalRect(posX, posY, 0, 0, xSize, ySize);
 		
@@ -83,16 +133,29 @@ public class GuiFurnaceBook extends GuiScreen {
 		drawTexturedModalRect(posX+110,posY+41,201,0,32,32);
 		
 		String title = "The Crafter's Guide to Mo Furnaces";
-		
+			
 		this.fontRendererObj.drawString(title, posX + this.xSize / 2
 				- this.fontRendererObj.getStringWidth(title) / 2, posY + 2, 4210752);
 		
 		buttonClose.drawButton(this.mc, 200,  200);
 		int k;
-		for (k = 1; k < buttonList.size(); k++) {
-			((GuiButton) buttonList.get(k)).drawButton(this.mc, 100, 10 + (20 * (k -1)));
+		for (k = 1; k < buttonListMenu.size(); k++) {
+			((GuiButton) buttonListMenu.get(k)).drawButton(this.mc, 100 + (50 * (k - 1)), 10);
 		}
 		
+		switch (index) {
+			case 1:
+			{
+				int j;
+				for (j = 0; j < buttonListItems.size(); j++) {
+					((GuiButton) buttonListItems.get(j)).drawButton(this.mc, 100, 10 + (20 * j));
+				}
+				break;
+			}
+			default:
+				break;
+		}
+		index = 0;
 		super.drawScreen(x,  y,  f);
     }
     
@@ -106,23 +169,40 @@ public class GuiFurnaceBook extends GuiScreen {
      */
 	@Override
     protected void mouseClicked(int p_73864_1_, int p_73864_2_, int p_73864_3_) {
+		GuiButton lastButton = null;
         if (p_73864_3_ == 0) {
-            for (int l = 0; l < this.buttonList.size(); ++l) {
-                GuiButton guibutton = (GuiButton)this.buttonList.get(l);
+            for (int l = 0; l < this.buttonListMenu.size(); ++l) {
+                GuiButton guibutton = (GuiButton)this.buttonListMenu.get(l);
 
                 if (guibutton.mousePressed(this.mc, p_73864_1_, p_73864_2_)) {
-                    ActionPerformedEvent.Pre event = new ActionPerformedEvent.Pre(this, guibutton, this.buttonList);
+                    ActionPerformedEvent.Pre event = new ActionPerformedEvent.Pre(this, guibutton, this.buttonListMenu);
                     if (MinecraftForge.EVENT_BUS.post(event))
                         break;
                     this.selectedButton = event.button;
                     event.button.func_146113_a(this.mc.getSoundHandler());
                     this.actionPerformed(event.button);
                     if (this.equals(this.mc.currentScreen))
-                        MinecraftForge.EVENT_BUS.post(new ActionPerformedEvent.Post(this, event.button, this.buttonList));
+                        MinecraftForge.EVENT_BUS.post(new ActionPerformedEvent.Post(this, event.button, this.buttonListMenu));
                     
+                    if (lastButton != null && lastButton == selectedButton){
+                    	System.out.println("buttons equal the same, returning with no action");
+                    	return;
+                    }
                     if (selectedButton == buttonClose) {
                     	this.mc.displayGuiScreen((GuiScreen)null);
                     	this.mc.setIngameFocus();
+                    }
+                    
+                    if (selectedButton == buttonItems) {
+                    	System.out.println("selected button is equal to last button");
+                    		lastButton = selectedButton;
+                        	index = 1;
+                        	this.drawScreen(xStored, yStored, fStored);
+
+                    } else if (selectedButton != buttonItems && selectedButton != buttonClose) {
+                    	System.out.println("selected button is not equal to the last button");
+                    	index = 0;
+                    	this.drawScreen(xStored, yStored, fStored);
                     }
                 }
             }
